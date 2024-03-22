411183324b3e89f286187b60e8dab26ba420c21a
rahulsainlll <rahulsain.social777@gmail.com>
2024-03-23 01:58:37 +0530

    Filter  ☑️

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -104,4 +104,33 @@ router.put("/", authMiddleware, async (req, res) => {
   });
 });
 
+// Filter
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
+
 module.exports = router;
