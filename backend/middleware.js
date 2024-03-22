badd70bf05230d69a1f98d80d9051dbc26cc7cfd
rahulsainlll <rahulsain.social777@gmail.com>
2024-03-23 00:33:54 +0530

    authMiddleware

---
+++ backend/middleware.js
@@ -0,0 +1,27 @@
+const { JWT_SECRET } = require("../config");
+
+const authMiddleware = (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.status(403).json({ error: "Missing or invalid auth header" });
+    return;
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.userId = decoded.userId;
+
+    next();
+  } catch (err) {
+    return res
+      .status(403)
+      .json({ error: `Error while decoded (midd.js): ${err}` });
+  }
+};
+
+module.exports = {
+  authMiddleware,
+};
